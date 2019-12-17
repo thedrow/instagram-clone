c10e8dded071e8e044cf60f550b18dc5ed7215f5
Omer Katz <omer.drow@gmail.com>
2019-12-17 12:48:04 +0200

    Progress...

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request,
+                  'website/index.html',
+                  {'name': 'Omer'})
